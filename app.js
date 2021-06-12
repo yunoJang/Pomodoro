f0ed4abb6e500219f3ba69fbc26e68a3cc9217cc
yuno <hanganda23@naver.com>
2021-06-12 16:53:43 +0900

    add Timer

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import './js/greeting.js';
 import './js/clock.js'
 import {show as showSetting} from './js/setting.js';
 import {show as showPomodoro} from './js/pomodoro.js';
+import './js/timer.js';
 
 
 function init() {
